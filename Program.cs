50d899f1aca437019ff4292f10354f14c3e04a59
Karson Nonya <thenosrak.exe@gmail.com>
2022-07-20 21:22:07 -0400

    init

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.Title = "TESTING";
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WindowHeight = 60;
+
+Console.WriteLine("Hello , Whats your name? (:  ");
+
+string userName = Console.ReadLine();
+
+Console.WriteLine ("My name is AI92357 , Your name is beutiful.");
+Console.WriteLine ("Whats your favorite color???");
+
+string userColor = Console.ReadLine();
+
+Console.WriteLine ("Mine is KAJNFKSANFKANGK, But i like that one too.");
+
+Console.ReadLine();
+
+Console.WriteLine ("on a scale of 1 - 10, How smart am i?");
+
+Console.ReadLine();
+
+Console.WriteLine ("Let me be honest, I dont actually know your name or color, Im just a dumb ai lol.");
+
+Console.ReadLine();
+
+Console.WriteLine ("Jk. I know your name, Its " + userName);
+
+Console.WriteLine ("Am i right?");
+
+Console.ReadLine();
+
+Console.WriteLine("i know your favorite color too!");
+Console.WriteLine("Its " + userColor);
+Console.WriteLine("right?");
+
+Console.ReadLine();
